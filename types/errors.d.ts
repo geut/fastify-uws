6495a3c2b42a35684ff0ba8a2789045e8e5d98c7
Martin Acosta <tinchoz49@gmail.com>
2023-11-05 10:53:00 -0300

    testing types

---
+++ types/errors.d.ts
@@ -0,0 +1,7 @@
+export const ERR_HEAD_SET: any;
+export const ERR_ADDRINUSE: any;
+export const ERR_UPGRADE: any;
+export const ERR_STREAM_DESTROYED: any;
+export const ERR_UWS_APP_NOT_FOUND: any;
+export const ERR_ENOTFOUND: any;
+export const ERR_SOCKET_BAD_PORT: any;
