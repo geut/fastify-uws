918d10b0b5aa589f65a6710b3fd25357ba9d603b
Martin Acosta <tinchoz49@gmail.com>
2024-07-13 10:45:23 -0300

    fix plugin types

--- scripts/generate-dts.js
+++ scripts/generate-dts.js
@@ -4,8 +4,11 @@ import { $ } from 'execa'
 
 await $`tsc src/server.js src/plugin.js --declaration --allowJs --emitDeclarationOnly --outDir types`
 
+const overload = fs.readFileSync('src/fastify-overload.d.ts', 'utf-8')
+
 const types = fs.readFileSync('types/plugin.d.ts', 'utf-8')
 fs.writeFileSync(
   'types/plugin.d.ts',
   types + 'import "./fastify-overload.d.ts"'
 )
+fs.writeFileSync('types/fastify-overload.d.ts', overload)
